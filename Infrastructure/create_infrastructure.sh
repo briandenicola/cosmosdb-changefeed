d552c28088adacae3bf6103618898776c41088e8
Brian Denicola <bjd145@yahoo.com>
2019-11-14 15:44:09 -0600

    Fixed bugs in deployment script

--- Infrastructure/create_infrastructure.sh
+++ Infrastructure/create_infrastructure.sh
@@ -6,26 +6,28 @@ export functionAppName=$3
 export cosmosDBAccountName=$4
 export storageAccountName=$5
 
-az login 
-az group create -n $RG -l $location
+#az login 
+az group create -n ${RG} -l ${location}
 
 #Create Storage Account 
-az storage account create --name $storageAccountName --location $location --resource-group $RG --sku Standard_LRS
-az storage container create -n documents --acount-name $storageAccountName
-$storageKey=`az storage account keys list -n $storageAccountName --query '[0].value' -o tsv`
-$storageConnectionString="DefaultEndpointsProtocol=https;AccountName=$storageAccountName;AccountKey=$storageKey"
+az storage account create --name ${storageAccountName} --location ${location} --resource-group ${RG} --sku Standard_LRS
+storageKey=`az storage account keys list -n ${storageAccountName} --query '[0].value' -o tsv`
+storageConnectionString='DefaultEndpointsProtocol=https;AccountName=${storageAccountName};AccountKey=${storageKey}'
+
+# Create Container
+az storage container create -n documents --account-name ${storageAccountName} --account-key ${storageKey}
 
 #Create Cosmos
-az cosmosdb create -g $RG -n $cosmosDBAccountName --kind GlobalDocumentDB 
-az cosmosdb database create  -g $RG -n $cosmosDBAccountName -d ToDoList
-az cosmosdb collection create -g $RG -n $cosmosDBAccountName -d ToDoList -c Items --partition-key-path '/_partitionKey'
-az cosmosdb collection create -g $RG -n $cosmosDBAccountName -d ToDoList -c leases --partition-key-path '/_partitionKey'
-$cosmosConnectionString=`az cosmosdb list-connection-strings -n $cosmosDBAccountName -g $RG --query 'connectionStrings[0].connectionString' -o tsv`
+az cosmosdb create -g ${RG} -n ${cosmosDBAccountName} --kind GlobalDocumentDB 
+az cosmosdb database create  -g ${RG} -n ${cosmosDBAccountName} -d ToDoList
+az cosmosdb collection create -g ${RG} -n ${cosmosDBAccountName} -d ToDoList -c Items --partition-key-path '/_partitionKey'
+az cosmosdb collection create -g ${RG} -n ${cosmosDBAccountName} -d ToDoList -c leases --partition-key-path '/_partitionKey'
+cosmosConnectionString=`az cosmosdb list-connection-strings -n ${cosmosDBAccountName} -g $RG --query 'connectionStrings[0].connectionString' -o tsv`
 
 #Create Function App
 funcStorageName=${functionAppName}sa
-az storage account create --name $funcStorageName --location $location --resource-group $RG --sku Standard_LRS
-az functionapp create --name $functionAppName --storage-account $funcStorageName --consumption-plan-location $location --resource-group $RG
+az storage account create --name ${funcStorageName} --location ${location} --resource-group ${RG} --sku Standard_LRS
+az functionapp create --name ${functionAppName} --storage-account ${funcStorageName} --consumption-plan-location ${location} --resource-group ${RG}
 
-az functionapp config appsettings set -g $RG -n $functionAppName --settings bjdchangefeed001_STORAGE=$storageConnectionString
-az functionapp config appsettings set -g $RG -n $functionAppName --settings bjdcosmos001_DOCUMENTDB=$cosmosConnectionString
+az functionapp config appsettings set -g ${RG} -n ${functionAppName} --settings bjdchangefeed001_STORAGE=${storageConnectionString}
+az functionapp config appsettings set -g ${RG} -n ${functionAppName} --settings bjdcosmos001_DOCUMENTDB=${cosmosConnectionString}
